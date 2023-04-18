2b033b066e1b1b27bc2bbb898efc2e9202be196f
Ryan Noronha <ryannor56a@proton.me>
2023-04-18 22:43:33 +0530

    fix(sway): update idle and suspend configs

---
+++ config/sway/scripts/suspend_locker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Script taken and modified from https://code.krister.ee/lock-screen-in-sway/
+
+# Times the suspend and puts it to background
+swayidle timeout 3 'suspend' &
+# Locks the screen immediately
+swaylock
+# Kills last background task so idle timer doesn't keep running
+kill %%
