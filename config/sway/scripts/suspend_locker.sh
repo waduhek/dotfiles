14ca521dae1e847febfbe6de3e7df70b21e4645f
Ryan Noronha <ryannor56a@proton.me>
2023-04-19 19:32:57 +0530

    fix(sway): fix broken suspend command

--- config/sway/scripts/suspend_locker.sh
+++ config/sway/scripts/suspend_locker.sh
@@ -3,7 +3,7 @@
 # Script taken and modified from https://code.krister.ee/lock-screen-in-sway/
 
 # Times the suspend and puts it to background
-swayidle timeout 3 'suspend' &
+swayidle timeout 3 'systemctl suspend' &
 # Locks the screen immediately
 swaylock
 # Kills last background task so idle timer doesn't keep running
