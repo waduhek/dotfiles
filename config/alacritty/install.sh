6e87932ffe84e681d4663f3b4d0f357376c8047d
Ryan Noronha <ryannor56a@proton.me>
2023-06-20 11:08:02 +0530

    fix(alacritty): fix incorrect destination in script

--- config/alacritty/install.sh
+++ config/alacritty/install.sh
@@ -11,7 +11,7 @@ app_config_dest=".config/alacritty"
 alacritty_theme="alacritty-theme"
 
 echo ">>> Copying themes"
-cp -rv "$(construct_path "$curr_dir" $app_dir $alacritty_theme)" "$(construct_path "$HOME" $app_config_dest $alacritty_theme)"
+cp -rv "$(construct_path "$curr_dir" $app_dir $alacritty_theme)" "$(construct_path "$HOME" $app_config_dest)"
 
 echo ">>> Copying alacritty.yml"
 cp -v "$(construct_path "$curr_dir" $app_dir "alacritty.yml")" "$(construct_path "$HOME" $app_config_dest "alacritty.yml")"
