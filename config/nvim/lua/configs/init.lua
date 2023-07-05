90f88c26d26b6d3d377f14492644a9055f216c98
Ryan Noronha <ryannor56a@proton.me>
2023-07-05 22:51:26 +0530

    fix(nvim): disable italics in `rose-pine` theme

--- config/nvim/lua/configs/init.lua
+++ config/nvim/lua/configs/init.lua
@@ -1,6 +1,7 @@
 -- Add all configs here
-require("configs.nvim_config")
 require("configs.lsp_config")
 require("configs.treesitter_config")
 require("configs.nvim_tree_config")
 require("configs.lualine_config")
+require("configs.rosepine_config")
+require("configs.nvim_config")
