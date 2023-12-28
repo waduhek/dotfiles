70d05c89f5bf38d96c70bad068c6b038a34d3825
Ryan Noronha <ryannor56a@proton.me>
2023-12-28 20:37:20 +0530

    refactor(alacritty): update alacritty config to use toml
    Update alacritty config to use TOML instead of YAML. Alacritty migrated
    to TOML in version 0.13.0

--- config/alacritty/install.sh
+++ config/alacritty/install.sh
@@ -13,5 +13,5 @@ alacritty_theme="alacritty-theme"
 echo ">>> Copying themes"
 cp -rv "$(construct_path "$curr_dir" $app_dir $alacritty_theme)" "$(construct_path "$HOME" $app_config_dest)"
 
-echo ">>> Copying alacritty.yml"
-cp -v "$(construct_path "$curr_dir" $app_dir "alacritty.yml")" "$(construct_path "$HOME" $app_config_dest "alacritty.yml")"
+echo ">>> Copying alacritty.toml"
+cp -v "$(construct_path "$curr_dir" $app_dir "alacritty.toml")" "$(construct_path "$HOME" $app_config_dest "alacritty.toml")"
