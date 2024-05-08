8904cc26a3d78a09a11675457c7367e6625748a2
Ryan Noronha <ryannor56a@proton.me>
2024-05-08 16:47:18 +0530

    refactor(nvim): use harpoon telescope extension
    Use harpoon telescope extension for marks.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -89,5 +89,8 @@ return require("lazy").setup({
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope.nvim",
         },
+        config = function ()
+            require("telescope").load_extension("harpoon")
+        end
     },
 })
