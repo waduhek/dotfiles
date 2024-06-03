e369185cac697aae7e02bb879edb902ad41dc5cc
Ryan Noronha <35768724+waduhek@users.noreply.github.com>
2024-06-03 12:53:11 +0530

    fix(nvim): update trouble maps for trouble v3 (#20)

--- config/nvim/lua/maps/trouble_maps.lua
+++ config/nvim/lua/maps/trouble_maps.lua
@@ -1,21 +1,17 @@
 -- Trouble.nvim keymaps
 vim.keymap.set(
-    "n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+    "n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",
     { silent = true, noremap = true }
 )
 vim.keymap.set(
-    "n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+    "n", "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
     { silent = true, noremap = true }
 )
 vim.keymap.set(
-    "n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+    "n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>",
     { silent = true, noremap = true }
 )
 vim.keymap.set(
-    "n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-    { silent = true, noremap = true }
-)
-vim.keymap.set(
-    "n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+    "n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>",
     { silent = true, noremap = true }
 )
