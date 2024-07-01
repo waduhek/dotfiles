3a74df28517800f3f4819284d627dfbb098e4993
Ryan Noronha <ryannor56a@proton.me>
2024-07-01 19:35:02 +0530

    refactor(sway): remove suspend locker script

--- config/sway/install.sh
+++ config/sway/install.sh
@@ -14,8 +14,3 @@ then
     mkdir "$(construct_path "$HOME" "$app_config_dest")"
 fi
 cp -v "$(construct_path "$curr_dir" "$app_dir" "config")" "$(construct_path "$HOME" "$app_config_dest")"
-
-echo ">>> Copying scripts folder"
-cp -rv "$(construct_path "$curr_dir" "$app_dir" "scripts")" "$(construct_path "$HOME" "$app_config_dest")"
-# shellcheck disable=SC2046
-chmod -v +x $(construct_path "$HOME" "$app_config_dest" "scripts" "*.sh")
