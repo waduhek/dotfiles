67980fe606f927c2560d5a4e81c0d3ad979d8184
Ryan Noronha <ryannor56a@proton.me>
2024-10-20 18:02:07 +0530

    fix: add hardware configurations file

---
+++ config/nixos/hardware-configuration.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+    imports = [
+        (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+    boot.initrd.availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "usb_storage"
+        "sd_mod"
+        "rtsx_pci_sdmmc"
+    ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+
+    filSystems = {
+        "/" = {
+            device = "/dev/disk/by-label/NixOS";
+            fsType = "ext4";
+        };
+
+        "/boot" = {
+            device = "/dev/disk/by-label/boot";
+            fsType = "vfat";
+            options = [ "fmask=0077" "dmask=0077" ];
+        };
+    };
+
+    swapDevices = [
+        { device = "/dev/disk/by-label/swap"; }
+    ];
+
+    networking.useDHCP = lib.mkDefault true;
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
