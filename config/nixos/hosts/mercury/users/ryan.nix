e37a771bb02422c937af8a9da20fefc51247658f
Ryan Noronha <ryannor56a@proton.me>
2024-10-29 18:38:07 +0530

    feat: add additional sway configuration to home module

--- config/nixos/hosts/mercury/users/ryan.nix
+++ config/nixos/hosts/mercury/users/ryan.nix
@@ -1,6 +1,55 @@
 { config, pkgs, ... }:
+let
+    swayPCConfig = ''
+        set $primary_display 'BNQ BenQ EX2510 5CL01826019'
+        set $secondary_display 'LG Electronics LG IPS FULLHD 0x00002D4C'
 
-{
+        bindsym $mod+m output $secondary_display toggle
+
+        #
+        # ==== Outputs ===
+        #
+
+        # Set the resolution and refresh rate of the monitor
+        output $primary_display {
+            mode 1920x1080@143.981Hz
+            position 0 0
+        }
+
+        output $secondary_display {
+            mode 1920x1080@74.973Hz
+            position 1920 0
+        }
+
+        #
+        # === Inputs ===
+        #
+
+        # Change the mouse sensitivity
+        input 1133:49291:Logitech_G502_HERO_Gaming_Mouse {
+            accel_profile flat
+            pointer_accel -0.9
+        }
+
+        #
+        # === Workspace Assignment ===
+        #
+
+        # Assign odd workspaces to primary display
+        workspace 1 output $primary_display
+        workspace 3 output $primary_display
+        workspace 5 output $primary_display
+        workspace 7 output $primary_display
+        workspace 9 output $primary_display
+
+        # Assign even workspaces to secondary display
+        workspace 2 output $secondary_display
+        workspace 4 output $secondary_display
+        workspace 6 output $secondary_display
+        workspace 8 output $secondary_display
+        workspace 10 output $secondary_display
+    '';
+in {
     imports = [
         ../../../modules/home
     ];
@@ -30,7 +79,10 @@
             alacritty.enable = true;
             tmux.enable = true;
         };
-        desktop.sway.enable = true;
+        desktop.sway = {
+            enable = true;
+            pcConfig = swayPCConfig;
+        };
         editor.neovim.enable = true;
     };
 }
--- config/nixos/modules/home/sway.nix
+++ config/nixos/modules/home/sway.nix
@@ -4,6 +4,12 @@ let
 in {
     options = {
         sys.desktop.sway.enable = lib.mkEnableOption "Enable sway configuration";
+        sys.desktop.sway.pcConfig = lib.mkOption {
+            type = lib.types.str;
+            description = "Custom config for individual hosts";
+            default = "";
+            example = "";
+        };
     };
 
     config = lib.mkIf cfg.enable {
@@ -12,6 +18,7 @@ in {
                 source = ../../../sway;
                 recursive = true;
             };
+            "sway/pc_config.conf".text = cfg.pcConfig;
             swaylock = {
                 source = ../../../swaylock;
                 recursive = true;
