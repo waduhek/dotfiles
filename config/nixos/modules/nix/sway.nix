a6a62eeeb8afbffbed321fc5656228b720b2b2e4
Ryan Noronha <ryannor56a@proton.me>
2024-10-31 21:31:26 +0530

    refactor: move packages installed by sway module to extra packages

--- config/nixos/modules/nix/sway.nix
+++ config/nixos/modules/nix/sway.nix
@@ -12,18 +12,18 @@ in {
         programs.sway = {
             enable = true;
             wrapperFeatures.gtk = true;
+            extraPackages = with pkgs; [
+                mako # Notification daemon for Sway.
+                grim # Screenshot utility.
+                slurp # Allows selecting portions of the screen.
+                wl-clipboard # Provide `wl-copy` and `wl-paste` commands for clipboard.
+                vulkan-validation-layers # For Vulkan support.
+                waybar # Replacement for swaybar.
+                swaylock # Screen locker.
+                swayidle # Idle manager.
+                bemenu # Menu library.
+                swaybg # Background manager.
+            ];
         };
-
-        environment.systemPackages = with pkgs; [
-            mako # Notification daemon for Sway.
-            grim # Screenshot utility.
-            slurp # Allows selecting portions of the screen.
-            wl-clipboard # Provide `wl-copy` and `wl-paste` commands for clipboard.
-            vulkan-validation-layers # For Vulkan support.
-            waybar # Replacement for swaybar.
-            swaylock # Screen locker.
-            bemenu # Menu library.
-            swaybg # Background manager.
-        ];
     };
 }
