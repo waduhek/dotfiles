942f9c2c79c201983e664d4eaf11ab3ba6cb2243
Ryan Noronha <ryannor56a@proton.me>
2024-11-09 10:20:01 +0530

    feat: enable sddm for plasma

--- config/nixos/modules/nix/plasma.nix
+++ config/nixos/modules/nix/plasma.nix
@@ -9,6 +9,7 @@ in {
     config = lib.mkIf cfg.enable {
         services.xserver.enable = true;
         services.desktopManager.plasma6.enable = true;
+        services.displayManager.sddm.enable = true;
 
         environment.plasma6.excludePackages = with pkgs.kdePackages; [
             plasma-browser-integration
