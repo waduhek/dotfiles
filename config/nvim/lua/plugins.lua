e5a52b1fd4f8932181f321c7e1c2f444d07f6143
Ryan Noronha <ryannor56a@proton.me>
2025-03-14 11:28:14 +0530

    chore: update `telescope.nvim` from 0.1.5 to 0.1.8

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -63,7 +63,7 @@ return require("lazy").setup({
     -- Picker for finding files, text, etc.
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
+        tag = "0.1.8",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope-live-grep-args.nvim",
