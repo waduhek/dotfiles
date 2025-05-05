c9e216a4630662e54cfe87c4fec36cffc93ed1b2
Ryan Noronha <ryannor56a@proton.me>
2025-05-05 23:35:27 +0530

    refactor(nvim): remove `lsp_lines.nvim` plugin

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -90,11 +90,4 @@ return require("lazy").setup({
             require("ibl").setup({ scope = { enabled = false } })
         end
     },
-
-    {
-        url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-        config = function ()
-            require("lsp_lines").setup()
-        end
-    },
 })
